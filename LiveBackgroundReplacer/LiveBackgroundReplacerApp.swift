1b386b9aecda7e1cbe9055caddffcddf4ae29750
David Arakelyan <arakelyan@trueconf.ru>
2024-08-21 15:01:42 +0300

    Initial Commit

---
+++ LiveBackgroundReplacer/LiveBackgroundReplacerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveBackgroundReplacerApp.swift
+//  LiveBackgroundReplacer
+//
+//  Created by David Arakelyan on 21.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct LiveBackgroundReplacerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
